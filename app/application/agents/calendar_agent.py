400110317731dac95b24a9942dae9a348eb1277e
manojkke <manojkke@gmail.com>
2025-03-09 15:30:11 +0800

    Updated the Calander agent

--- app/application/agents/calendar_agent.py
+++ app/application/agents/calendar_agent.py
@@ -6,13 +6,14 @@ class CalendarAgent(Agent):
     def __init__(self, calendar_service: CalendarService):
         self.calendar_service = calendar_service
 
-    async def handle_query(self, userChatQuery: str) -> str:
+    async def handle_query(self, userChatQuery: str, userChatHistory: str) -> str:
         # Calendar-specific processing here
         # metingDetails = parse_meeting_details(userChatQuery)
 
         # result = self.calendar_service.schedule_event(metingDetails.title, metingDetails.location,
         #                                               metingDetails.description, metingDetails.start_date,  metingDetails.end_date,
         #                                               metingDetails.reminders, metingDetails.timezone, metingDetails.attendees)
+
         result = self.calendar_service.schedule_event('Meeting with John', '1234 Street, City, Country',
                                                       'Discussing the quarterly goals and progress.', '2025-03-05T09:00:00',
                                                       '2025-03-05T10:00:00', True, 'America/New_York', ['manojkke@gmail.comn', 'samit@gmail.com'])
